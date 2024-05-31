291c7406b6c2c90b2417ab4c8185f1140d848847
danielemantovani <daniele.mantovani124@gmail.com>
2024-05-31 16:39:14 +0200

    fix: construct

--- Models/film.php
+++ Models/film.php
@@ -1,15 +1,15 @@
 <?php
-class film{
+class Film{
     public string $titolo;
     public string $nazione;
     public string $durata;
     private string $contenuto_per_adulti;
 
-    public function __construct( string $titolo, string $nazione, string $durata, string $contenuto_per_adulti )
+    public function __construct( string $titolo, string $nazione, string $durata)
     {
-        $this -> $titolo = $titolo;
-        $this -> $nazione = $nazione;
-        $this -> $durata = $durata;
+        $this-> titolo = $titolo;
+        $this-> nazione = $nazione;
+        $this-> durata = $durata;
     }
 }
 ?>
--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
-    
+    require_once "Models/film.php";
+
+    $film1 = new Film ("Altrimenti ci arrabbiamo", "Usa", "110");
+
+    var_dump($film1)
 
 ?>
